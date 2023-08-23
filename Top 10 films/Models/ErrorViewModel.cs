ddf3fa2b23ac5cf0c84634ab89d8e8fde142db52
blazhkevych.v_2 <blazhkevych.v@gmail.com>
2023-08-23 16:02:44 +0300

    #Code Formatting

--- Top 10 films/Models/ErrorViewModel.cs
+++ Top 10 films/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Top_10_films.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Top_10_films.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }

03619a7a1eee51e5f4042c0cc41ee3ca6f93ff8b
blazhkevych.v_2 <blazhkevych.v@gmail.com>
2023-08-31 12:29:04 +0300

    #img check

--- Top 10 films/Models/ErrorViewModel.cs
+++ Top 10 films/Models/ErrorViewModel.cs
@@ -1,12 +1,13 @@
-namespace Top_10_films.Models;
+//namespace Top_10_films.Models;
 
-// Define the ErrorViewModel class
-public class ErrorViewModel
-{
-    // Declare a nullable string property for the RequestId
-    public string? RequestId { get; set; }
+//// Define the ErrorViewModel class
+//public class ErrorViewModel
+//{
+//    // Declare a nullable string property for the RequestId
+//    public string? RequestId { get; set; }
 
-    // Declare a boolean property ShowRequestId
-    // This property returns true if RequestId is not null or empty
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
+//    // Declare a boolean property ShowRequestId
+//    // This property returns true if RequestId is not null or empty
+//    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+//}
+
--- Top 10 films/Models/MovieContext.cs
+++ Top 10 films/Models/MovieContext.cs
@@ -25,7 +25,7 @@ public class MovieContext : DbContext
                 Director = "Frank Darabont",
                 Genre = "Drama",
                 ReleaseYear = 1994,
-                PosterUrl = "~/img/The_Shawshank_Redemption_1994.jpg",
+                PosterUrl = "\\img\\The_Shawshank_Redemption_1994.jpg",
                 Description =
                     "Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion."
             });
@@ -36,7 +36,7 @@ public class MovieContext : DbContext
                 Director = "Francis Ford Coppola",
                 Genre = "Crime",
                 ReleaseYear = 1972,
-                PosterUrl = "img\\The_Godfather_(1972).jpg",
+                PosterUrl = "\\img\\The_Godfather_(1972).jpg",
                 Description =
                     "Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger."
             });
@@ -47,7 +47,7 @@ public class MovieContext : DbContext
                 Director = "Christopher Nolan",
                 Genre = "Action",
                 ReleaseYear = 2008,
-                PosterUrl = "img\\The_Dark_Knight_(2008).jpg",
+                PosterUrl = "\\img\\The_Dark_Knight_(2008).jpg",
                 Description =
                     "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
             });
@@ -58,7 +58,7 @@ public class MovieContext : DbContext
                 Director = "Francis Ford Coppola",
                 Genre = "Crime",
                 ReleaseYear = 1974,
-                PosterUrl = "img\\The_Godfather_Part_II_(1974).jpg",
+                PosterUrl = "\\img\\The_Godfather_Part_II_(1974).jpg",
                 Description =
                     "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate."
             });
@@ -69,7 +69,7 @@ public class MovieContext : DbContext
                 Director = "Sidney Lumet",
                 Genre = "Drama",
                 ReleaseYear = 1957,
-                PosterUrl = "img\\12_Angry_Men_(1957).jpg",
+                PosterUrl = "\\img\\12_Angry_Men_(1957).jpg",
                 Description =
                     "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence."
             });
@@ -80,7 +80,7 @@ public class MovieContext : DbContext
                 Director = "Steven Spielberg",
                 Genre = "Drama",
                 ReleaseYear = 1993,
-                PosterUrl = "img\\Schindler's_List_(1993).jpg",
+                PosterUrl = "\\img\\Schindler's_List_(1993).jpg",
                 Description =
                     "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis."
             });
@@ -91,7 +91,7 @@ public class MovieContext : DbContext
                 Director = "Peter Jackson",
                 Genre = "Adventure",
                 ReleaseYear = 2003,
-                PosterUrl = "img\\The_Lord_of_the_Rings_The_Return_of_the_King_(2003).jpg",
+                PosterUrl = "\\img\\The_Lord_of_the_Rings_The_Return_of_the_King_(2003).jpg",
                 Description =
                     "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
             });
@@ -102,7 +102,7 @@ public class MovieContext : DbContext
                 Director = "Quentin Tarantino",
                 Genre = "Crime",
                 ReleaseYear = 1994,
-                PosterUrl = "img\\Pulp_Fiction_(1994).jpg",
+                PosterUrl = "\\img\\Pulp_Fiction_(1994).jpg",
                 Description =
                     "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
             });
@@ -113,7 +113,7 @@ public class MovieContext : DbContext
                 Director = "Peter Jackson",
                 Genre = "Adventure",
                 ReleaseYear = 2001,
-                PosterUrl = "img\\The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_(2001).jpg",
+                PosterUrl = "\\img\\The_Lord_of_the_Rings_The_Fellowship_of_the_Ring_(2001).jpg",
                 Description =
                     "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron."
             });
@@ -124,7 +124,7 @@ public class MovieContext : DbContext
                 Director = "Sergio Leone",
                 Genre = "Western",
                 ReleaseYear = 1966,
-                PosterUrl = "~/img/The_Good_the_Bad_and_the_Ugly_1966.jpg",
+                PosterUrl = "\\img\\The_Good_the_Bad_and_the_Ugly_1966.jpg",
                 Description =
                     "A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery."
             });
